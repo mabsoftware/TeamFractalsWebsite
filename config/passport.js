3a6a7862f19f460e10190eedcd196d5cbf9216a0
Max Bowman <maxabowman@gmail.com>
2016-12-22 19:46:56 -0600

    Fixed a big problem...

--- config/passport.js
+++ config/passport.js
@@ -45,23 +45,23 @@ module.exports = function(passport) {
 
     }));
 
-};
-
-passport.use('local-login', new LocalStrategy({
+    passport.use('local-login', new LocalStrategy({
         usernameField : 'email',
         passwordField : 'password',
         passReqToCallback : true,
-    function(req, email, password, done) {
-        User.findOne({ 'local.email' :  email }, function(err, user) {
-            if (err) return done(err);
+        function(req, email, password, done) {
+            User.findOne({ 'local.email' :  email }, function(err, user) {
+                if (err) return done(err);
 
-            if (!user)
-                return done(null, false, req.flash('loginMessage', 'No matching user.'));
+                if (!user)
+                    return done(null, false, req.flash('loginMessage', 'No matching user.'));
 
-            if (!user.validPassword(password))
-                return done(null, false, req.flash('loginMessage', 'Incorrect password.'));
+                if (!user.validPassword(password))
+                    return done(null, false, req.flash('loginMessage', 'Incorrect password.'));
 
-            return done(null, user);
-        });
+                return done(null, user);
+            });
 
-    });
+        }));
+
+};
