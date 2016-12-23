a0ace9816dfaa2fb1b993dea2cbe129d4035afb1
Max Bowman <maxabowman@gmail.com>
2016-12-22 19:43:23 -0600

    Fixed bug.

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,4 @@
 var LocalStrategy = require('passport-local').Strategy;
-
 var User = require('../app/models/user');
 
 module.exports = function(passport) {
@@ -51,16 +50,16 @@ module.exports = function(passport) {
 passport.use('local-login', new LocalStrategy({
         usernameField : 'email',
         passwordField : 'password',
-        passReqToCallback : true
+        passReqToCallback : true,
     function(req, email, password, done) {
         User.findOne({ 'local.email' :  email }, function(err, user) {
             if (err) return done(err);
 
             if (!user)
-                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                return done(null, false, req.flash('loginMessage', 'No matching user.'));
 
             if (!user.validPassword(password))
-                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                return done(null, false, req.flash('loginMessage', 'Incorrect password.'));
 
             return done(null, user);
         });
