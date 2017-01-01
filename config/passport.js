7f5a1d472515a5c829c2e857c13bee6d9879f248
Max Bowman <maxabowman@gmail.com>
2017-01-01 08:23:32 -0600

    Updates.

--- config/passport.js
+++ config/passport.js
@@ -25,6 +25,7 @@ module.exports = function(passport) {
             if (user) {
                 return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
             }
+            
             else {
 
                 var newUser = new User();
@@ -53,15 +54,9 @@ module.exports = function(passport) {
     },
     function(req, email, password, done) {
         User.findOne({ 'local.email' :  email }, function(err, user) {
-            if (err)
-                return done(err);
-
-            if (!user)
-                return done(null, false, req.flash('loginMessage', 'No user found.'));
-
-            if (!user.validPassword(password))
-                return done(null, false, req.flash('loginMessage', 'Wrong password.'));
-
+            if (err) return done(err);
+            if (!user) return done(null, false, req.flash('loginMessage', 'No user found.'));
+            if (!user.validPassword(password)) return done(null, false, req.flash('loginMessage', 'Wrong password.'));
             return done(null, user);
         });
 
