31779a7d86add9aa06b88311d00ad699b3e7b493
Max Bowman <maxabowman@gmail.com>
2016-12-31 19:28:14 -0600

    Create scout account.

--- config/passport.js
+++ config/passport.js
@@ -31,7 +31,7 @@ module.exports = function(passport) {
 
                 newUser.local.email = email;
                 newUser.local.password = newUser.generateHash(password);
-                newUser.isScout = false; // people who are signing up aren't our team scout.
+                newUser.isScout = true; // people who are signing up aren't our team scout.
                 // team scout will be created through the mongo shell.
 
                 newUser.save(function(err) {
