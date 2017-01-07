c47e58c1924e967ef1d2798ac729e96174fbe7e6
Max Bowman <maxabowman@gmail.com>
2017-01-07 14:14:23 -0600

    added.

---
+++ app/models/contact.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+var contactSchema = mongoose.Schema({
+    from: String,
+    subject: String,
+    message: String
+});
+
+module.exports = mongoose.model('Contact', contactSchema);
